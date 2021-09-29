1469831c7c3466c982765ac18386068497b0d066
Utku Cakir <utkucakirr@gmail.com>
2021-09-29 14:24:51 +0300

    Homework 1 completed.

---
+++ Java101/src/VucutKitleIndeksi.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class VucutKitleIndeksi {
+    public static void main(String[] args) {
+        double boy,kg,indeks;
+        Scanner inp=new Scanner(System.in);
+
+        System.out.print("Boyunuzu metre cinsinden giriniz: ");
+        boy=inp.nextDouble();
+
+        System.out.print("Kilonuzu giriniz: ");
+        kg=inp.nextDouble();
+
+        indeks=kg/(boy*boy);
+        System.out.println("Vucut kitle indeksiniz: " + indeks);
+    }
+}
