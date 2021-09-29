208a353cd74aecb0f668248edb06c20f880da964
Utku Cakir <utkucakirr@gmail.com>
2021-09-29 14:07:31 +0300

    Practice 3 completed.

---
+++ Java101/src/UcgeninAlani.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class UcgeninAlani {
+    public static void main(String[] args) {
+        double kenar1,kenar2,kenar3;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Ucgenin 1. kenarinin uzunlugunu giriniz: ");
+        kenar1=input.nextDouble();
+
+        System.out.print("Ucgenin 2. kenarinin uzunlugunu giriniz: ");
+        kenar2=input.nextDouble();
+
+        System.out.print("Ucgenin 3. kenarinin uzunlugunu giriniz: ");
+        kenar3=input.nextDouble();
+
+        double alan,u;
+        u=(kenar1+kenar2+kenar3)/2;
+        alan=Math.sqrt(u*(u-kenar1)*(u-kenar2)*(u-kenar3));
+
+        System.out.println("Ucgenin alani: " +alan);
+
+    }
+}
