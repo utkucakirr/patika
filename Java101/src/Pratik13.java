104ad5709f2a67c590200ca7e7328b7233a08ed9
Utku Cakir <utkucakirr@gmail.com>
2021-10-01 23:54:29 +0300

    Practice 13 completed.

---
+++ Java101/src/Pratik13.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Pratik13 {
+    public static void main(String[] args) {
+        int toplam=0,sayi;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Bir sayi giriniz: ");
+        sayi=inp.nextInt();
+        if(sayi%4==0){
+            toplam+=sayi;
+        }
+        while(sayi%2==0){
+            System.out.print("Sayi giriniz: ");
+            sayi=inp.nextInt();
+            if(sayi%4==0){
+                toplam+=sayi;
+            }
+        }
+        System.out.println(toplam);
+    }
+}
