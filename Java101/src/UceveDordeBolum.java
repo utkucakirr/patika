c9e68acdea887ed16f34657cd6f60ee7b8486849
Utku Cakir <utkucakirr@gmail.com>
2021-10-01 23:46:53 +0300

    Practice 12 completed.

---
+++ Java101/src/UceveDordeBolum.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class UceveDordeBolum {
+    public static void main(String[] args) {
+        int sayi,toplam=0,count=0;
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Bir sayi giriniz: ");
+        sayi=inp.nextInt();
+        for(int i=1;i<sayi;i++){
+            if(i%3==0&&i%4==0){
+                count=count+1;
+                toplam=toplam+i;
+            }
+        }
+        double ortalama;
+        if(count==0){
+            System.out.println("Bulunamadi!");
+        }else{
+            ortalama = toplam/count;
+            System.out.println(toplam);
+            System.out.println(count);
+            System.out.println("Ortalama= " + ortalama);
+        }
+
+
+    }
+}
