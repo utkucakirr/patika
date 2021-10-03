d4d00972bce6776f8643ece044783c6412552e7b
Utku Cakir <utkucakirr@gmail.com>
2021-10-03 23:27:08 +0300

    Practice 18 completed.

---
+++ Java101/src/HarmonikSeri.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class HarmonikSeri {
+    public static void main(String[] args) {
+        int n;
+        double sonuc=0.0;
+        Scanner input=new Scanner(System.in);
+        System.out.print("n degerini giriniz: ");
+        n=input.nextInt();
+        for(double i=1;i<=n;i++){
+            sonuc=sonuc+(1/i);
+        }
+        System.out.println("Sonuc= "+sonuc);
+    }
+}
