309eceb2402b563e5ba23c87a5e85d0d5ec44935
Utku Cakir <utkucakirr@gmail.com>
2021-10-03 23:44:38 +0300

    Practice 19 completed.

---
+++ Java101/src/Yildiz.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Yildiz {
+    public static void main(String[] args) {
+        int n;
+        Scanner input=new Scanner(System.in);
+        System.out.print("n degerini giriniz: ");
+        n= input.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=n-i;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=(2*i-1);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<(2*n)-(2*i);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
