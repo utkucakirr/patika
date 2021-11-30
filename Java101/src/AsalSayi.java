fa386336d14e4316a7a1bc0c2cf461cd51df2a6c
Utku Cakir <utkucakirr@gmail.com>
2021-11-30 13:23:07 +0300

    Homework 9 completed.

--- Java101/src/AsalSayi.java
+++ Java101/src/AsalSayi.java
@@ -3,11 +3,22 @@ import java.util.Scanner;
 public class AsalSayi {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        int n,prime=1;
+        int n;
         System.out.print("n: ");
         n=input.nextInt();
+        for(int i=3;i<=n;i++){
+            int prime=1;
+            for(int j=2;j<i;j++){
+                if(i%j==0){
+                    prime=0;
+                }
+            }
+            if(prime!=0){
+                System.out.print(i + ", ");
+            }
+        }
 
-        
+        input.close();
     }
     
 }
