37bb7a82b93de8d08adf1752e1c250d2e95a9b8f
Utku Cakir <utkucakirr@gmail.com>
2021-11-30 13:01:06 +0300

    Homework 8 completed.

---
+++ Java101/src/TersUcgen.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+        int n;
+        Scanner input=new Scanner(System.in);
+        System.out.print("n: ");
+        n=input.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int l=0;l<=2*n-2*i;l++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
