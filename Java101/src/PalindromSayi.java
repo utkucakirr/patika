30443ae8bcbfe47cf8a553b4448d555afb21b159
Utku Cakir <utkucakirr@gmail.com>
2021-11-30 14:23:12 +0300

    Practice 22 completed.

---
+++ Java101/src/PalindromSayi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PalindromSayi {
+    static void IsPalindrome(int number){
+        int temp=number,sum=0;
+        while(temp>0){
+            int b=temp%10;
+            sum=sum*10+b;
+            temp=temp/10;
+        }
+        if(sum==number){
+            System.out.print(number + " palindrom sayidir.");
+        }else{
+            System.out.print(number + " palindrom sayi degildir.");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("n: ");
+        int n=input.nextInt();
+        IsPalindrome(n);
+    }
+}
